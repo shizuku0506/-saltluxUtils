001c370e2e32f2b5a711b87e95aef00cfde9e11a
woong8609 <woong8609@nate.com>
2014-12-09 11:59:54 +0900

    DB Setting and log4j log file create.

--- SingleWeb/src/main/java/kr/pe/singleweb/HomeController.java
+++ SingleWeb/src/main/java/kr/pe/singleweb/HomeController.java
@@ -17,24 +17,31 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController
 {
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
 	/**
-	 * Simply selects the home view to render by returning its name.
+	 * logger 설명 및 파라미터 설명
 	 */
-	@RequestMapping(value = "/index111.do", method = RequestMethod.GET)
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * @param locale
+	 * @param model
+	 * @return
+	 *
+	 * @author 강인웅 e-mail: woong8609@nate.com
+	 * @since 2014. 12. 8.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model)
 	{
 		logger.info("Welcome home! The client locale is {}.", locale);
-
+		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
+		
 		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate);
-
-		return "/WEB-INF/views/test/{1}.jsp";
+		
+		model.addAttribute("serverTime", formattedDate );
+		return "index";
 	}
-
+	
 }
