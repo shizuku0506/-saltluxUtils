7f493a8ac6ff1965cf21f3b0c236373dcddb4e65
woong8609 <woong8609@nate.com>
2014-12-10 13:22:29 +0900

    tiles 2 setting.

--- SingleWeb/src/main/java/kr/pe/singleweb/HomeController.java
+++ SingleWeb/src/main/java/kr/pe/singleweb/HomeController.java
@@ -18,13 +18,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController
 {
 	/**
-	 * logger 설명 및 파라미터 설명
+	 * logger : logger instance generation.
 	 */
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
-	 * @param locale
-	 * @param model
+	 * @param locale : 지역화 지정 변수
+	 * @param model : 
 	 * @return
 	 *
 	 * @author 강인웅 e-mail: woong8609@nate.com
@@ -34,14 +34,15 @@ public class HomeController
 	public String home(Locale locale, Model model)
 	{
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		return "index";
+
+		model.addAttribute("serverTime", formattedDate);
+		// return "index";
+		return "index.tiles";
 	}
-	
+
 }
