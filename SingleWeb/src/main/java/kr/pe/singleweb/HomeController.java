7d6e2e799d7cd9644df17837db2fe9fcd3fc3f7d
souler2585 <souler2585@gmail.com>
2014-12-11 17:28:38 -0800

    jackson setting

--- SingleWeb/src/main/java/kr/pe/singleweb/HomeController.java
+++ SingleWeb/src/main/java/kr/pe/singleweb/HomeController.java
@@ -1,20 +1,24 @@
 package kr.pe.singleweb;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
+@RequestMapping(value = "/")
 public class HomeController
 {
 	/**
@@ -23,17 +27,19 @@ public class HomeController
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	/**
-	 * @param locale : 지역화 지정 변수
-	 * @param model : 
+	 * @param locale
+	 *            : 지역화 지정 변수
+	 * @param model
+	 *            :
 	 * @return
 	 *
 	 * @author 강인웅 e-mail: woong8609@nate.com
 	 * @since 2014. 12. 8.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "index.do")
 	public String home(Locale locale, Model model)
 	{
-		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.debug("Welcome home! The client locale is {}.", locale);
 
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -43,6 +49,33 @@ public class HomeController
 		model.addAttribute("serverTime", formattedDate);
 		// return "index";
 		return "index.tiles";
+	}
+
+	/**
+	 * json 값 테스트
+	 *
+	 * @author 박정선 e-mail: souler2585@gmail.com
+	 * @since 2014. 12. 11.
+	 */
+	@RequestMapping(value = "test.do")
+	@ResponseBody
+	public ModelMap testJson()
+	{
+		logger.debug("json Test");
+
+		ModelMap modelMap = new ModelMap();
+
+		List<String> list = new ArrayList<String>();
+		list.add("111");
+		list.add("222");
+		list.add("333");
+		list.add("444");
+		list.add("555");
+
+		modelMap.addAttribute("testString", "testString");
+		modelMap.addAttribute("list", list);
+
+		return modelMap;
 	}
 
 }
